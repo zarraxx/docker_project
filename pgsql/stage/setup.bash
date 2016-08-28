8b91b1e67fa8798d22424e689f1fd81314dc4049
User <zarra@gcp.c.zarraxx.internal>
2016-08-28 00:33:27 +0000

    9

--- pgsql/stage/setup.bash
+++ pgsql/stage/setup.bash
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-echo hello
-
-
 apt-get update -y
 apt-get upgrade -y
 apt-get install -y build-essential
 apt-get install -y libsybdb5 freetds-dev freetds-common
-apt-get install -y  postgresql-server-dev-9.5
+apt-get install -y postgresql-server-dev-9.5
+apt-get install -y postgresql-9.5-python-multicorn
+apt-get install -y python-pip python-dev
+
+pip install python-tds
+pip install bitarray
+
+pip install PyMySQL
+
+pip install pg8000
+
+
 
 echo `pwd`
 tar -xvzf /stage/v1.0.7.tar.gz
@@ -15,4 +23,5 @@ cd tds_fdw-1.0.7
 make USE_PGXS=1
 make USE_PGXS=1 install
 cd -
+
 cp /stage/locales.conf /etc/freetds
