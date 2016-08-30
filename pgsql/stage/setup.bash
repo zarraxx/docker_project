b6d5408cd416358df3d2b20823331b21160dec9b
User <zarra@gcp.c.zarraxx.internal>
2016-08-30 04:55:20 +0000

    9

--- pgsql/stage/setup.bash
+++ pgsql/stage/setup.bash
@@ -6,8 +6,8 @@ apt-get install -y vim build-essential
 apt-get install -y unixodbc-dev libmyodbc  odbc-postgresql
 apt-get install -y libsybdb5 tdsodbc  freetds-bin freetds-dev freetds-common
 apt-get install -y postgresql-server-dev-9.5
-apt-get install -y postgresql-plpython3-9.5 postgresql-9.5-python3-multicorn
-apt-get install -y python3-pip python3-dev
+apt-get install -y postgresql-plpython-9.5 postgresql-plpython3-9.5 postgresql-9.5-python3-multicorn
+apt-get install -y python3-pip python3-dev python-pip python-dev
 
 pip3 install pymssql
 
@@ -16,6 +16,12 @@ pip3 install PyMySQL
 pip3 install pg8000
 
 
+pip install pymssql
+
+pip install PyMySQL
+
+pip install pg8000
+
 
 echo `pwd`
 tar -xvzf /stage/v1.0.7.tar.gz
@@ -23,7 +29,7 @@ cd tds_fdw-1.0.7
 make USE_PGXS=1
 make USE_PGXS=1 install
 cd -
-
+rm -rf tds_fdw-1.0.7
 
 tar xzvf /stage/odbc_fdw.0.1.0-rc1.tar.gz 
 cd odbc_fdw-0.1.0-rc1
@@ -31,6 +37,7 @@ make
 make install 
 
 cd -
+rm -rf odbc_fdw-0.1.0-rc1
 
 cp /stage/locales.conf /etc/freetds
 cp /stage/odbcinst.ini  /etc
