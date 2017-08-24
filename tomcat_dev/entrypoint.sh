9f4ab65d579e2a38891ad99725f611146796a4a4
User <zarra@develop01.openstacklocal>
2017-08-25 00:04:41 +0800

    abc

--- tomcat_dev/entrypoint.sh
+++ tomcat_dev/entrypoint.sh
@@ -4,13 +4,15 @@ if [ ! -n "$GIT_URL" ]; then
   echo "GIT_URL not set"
 else
   echo "Clone from $GIT_URL"
+  PREV=`pwd`
   mkdir -p /tmp/git
   rm -rf /tmp/git
   mkdir -p /tmp/git
   cd /tmp/git
   git clone $GIT_URL CONF
-  cd CONF$LOCAL
+  cd CONF$CONF_DIR
   sh run.sh
+  cd $PREV
 fi
 
 
