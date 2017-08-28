16128918cf6dc4f545ad9715a026286a02350aad
zarra <xsp@xyan.net.cn>
2017-08-28 17:40:05 +0800

    add pptpVPN

--- pptp-client/start.sh
+++ pptp-client/start.sh
@@ -2,4 +2,4 @@
 
 sysctl net.ipv4.ip_forward=1
 
-pptpsetup --create pptpVPN --server $SERVER --username $USER--password $PWD --encrypt --start
+#pptpsetup --create pptpVPN --server $SERVER --username $USER--password $PWD --encrypt --start
